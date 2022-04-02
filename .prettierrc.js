0c670f2c87d837cafef5b2842cbc37c917b087f7
luisescx <luis.esculapio@live.com>
2022-04-01 21:07:04 -0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: true,
+};
