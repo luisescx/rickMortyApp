0edb2fc54c1db76c1e727b2805376282985bd618
luisescx <luis.esculapio@live.com>
2022-04-03 14:14:34 -0300

    feat: added episodes screens

---
+++ src/scenes/Episodes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import {Container} from './styles';
+
+const Episodes = () => {
+  return (
+    <Container>
+      <Text>Episodes</Text>
+    </Container>
+  );
+};
+
+export default Episodes;
