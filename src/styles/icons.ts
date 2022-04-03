bcc97e3c95d8b5807b122d2ef528b643f77d4f3e
luisescx <luis.esculapio@live.com>
2022-04-03 12:03:30 -0300

    feat: added icons styles

---
+++ src/styles/icons.ts
@@ -0,0 +1,9 @@
+import Characters from '@/assets/icons/characters.svg';
+import Episodes from '@/assets/icons/episodes.svg';
+
+const icons = {
+  characters: Characters,
+  episodes: Episodes,
+};
+
+export default icons;
