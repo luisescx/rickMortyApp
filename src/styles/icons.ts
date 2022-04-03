daa6387cfbd684e67dac554028ffc395d18474fe
luisescx <luis.esculapio@live.com>
2022-04-03 19:21:05 -0300

    chore: added filter icon

--- src/styles/icons.ts
+++ src/styles/icons.ts
@@ -1,9 +1,11 @@
 import Characters from '@/assets/icons/characters.svg';
 import Episodes from '@/assets/icons/episodes.svg';
+import Filter from '@/assets/icons/filter.svg';
 
 const icons = {
   characters: Characters,
   episodes: Episodes,
+  filter: Filter,
 };
 
 export default icons;
