a05e4d6d44d3d7c6b5d6af7e92e1c840731e5c73
luisescx <luis.esculapio@live.com>
2022-04-03 19:21:26 -0300

    chore: added new components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,7 @@
 export {default as Input} from './Input';
 export {default as CharacterCard} from './CharacterCard';
+export {default as ModalStyled} from './ModalStyled';
+export {default as Radio} from './Radio';
+export {default as Filter} from './Filter';
+export {default as Picker} from './Picker';
+export {default as Loading} from './Loading';
