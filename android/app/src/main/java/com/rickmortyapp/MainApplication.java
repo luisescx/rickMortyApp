214a8deb1abc21fbaa33133b4da2a996322c0d63
luisescx <luis.esculapio@live.com>
2022-04-03 11:59:42 -0300

    chore: added reanimated on java main application

--- android/app/src/main/java/com/rickmortyapp/MainApplication.java
+++ android/app/src/main/java/com/rickmortyapp/MainApplication.java
@@ -14,6 +14,9 @@ import com.rickmortyapp.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.facebook.react.bridge.JSIModulePackage; // <- add
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -35,6 +38,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage();
         }
       };
 
