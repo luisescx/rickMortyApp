8a784e7e4bca27bf6d35518cc040f8ad50a00f15
luisescx <luis.esculapio@live.com>
2022-04-04 09:18:39 -0300

    chore: added icons on both plataforms

--- ios/rickMortyApp/AppDelegate.mm
+++ ios/rickMortyApp/AppDelegate.mm
@@ -7,6 +7,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"
 
 #import <React/RCTAppSetupUtils.h>
 
@@ -63,6 +64,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
