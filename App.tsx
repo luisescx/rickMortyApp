3ada9e1db90f84a0a2778eb66e771363a5d1c532
luisescx <luis.esculapio@live.com>
2022-04-04 03:06:38 -0300

    .

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import Navigation from '@/navigations';
 import Contexts from '@/contexts';
+import {LogBox} from 'react-native';
+
+LogBox.ignoreAllLogs();
 
 const App = () => {
   return (
